519eb3a860c736d2e7df846e21fb4d949b163323
Kim Rasmussen <kimera@stud.ntnu.no>
2019-02-13 11:35:59 +0100

    database handler inserted

--- src/main/java/utilities/DBHandler.java
+++ src/main/java/utilities/DBHandler.java
@@ -1,16 +1,16 @@
 package utilities;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 
 public class DBHandler {
-    String username = "kimera_k3ah";
-    String password = "passord132";
-    String url = "mysql.stud.ntnu.no";
-    String driver = "com.mysql.jdbc.Driver";
+    private final String username = "kimera_k3ah";
+    private final String  password = "passord132";
+    private final String url = "mysql.stud.ntnu.no";
+    private final String driver = "com.mysql.jdbc.Driver";
 
-    public Connection getConnection() throws SQLException{
+
+    public Connection getConnection(){
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, username, password);
@@ -20,6 +20,54 @@ public class DBHandler {
         }
             System.out.println("Connected to database");
         return conn;
+
+    }
+
+    public List<String> selectAllFromTable(String table) {
+        Connection conn = getConnection();
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT foo FROM bar");
+
+            // or alternatively, if you don't know ahead of time that
+            // the query will be a SELECT...
+
+            if (stmt.execute("SELECT foo FROM bar")) {
+                rs = stmt.getResultSet();
+            }
+            return null;
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            // it is a good idea to release
+            // resources in a finally{} block
+            // in reverse-order of their creation
+            // if they are no-longer needed
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+
+                rs = null;
+            }
+
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+
+                stmt = null;
+            }
+        }
 
     }
 }
