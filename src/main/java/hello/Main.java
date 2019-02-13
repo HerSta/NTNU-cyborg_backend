5d694039032a278ef525bd9f0c9cc13971f398bb
Kim Rasmussen <kimera@stud.ntnu.no>
2019-02-13 14:57:22 +0100

    inital database functions implemented, added driver for mysql in pom file.

--- src/main/java/hello/Main.java
+++ src/main/java/hello/Main.java
@@ -2,21 +2,18 @@ package hello;
 import utilities.DBHandler;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try {
 
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-        } catch (Exception ex) {
-            System.out.println("Could not find driver for database");
-        }
+    public static void main(String[] args) {
 
         DBHandler dbHandler = new DBHandler();
-
+        String file = "C:\\Users\\Kim Erling\\Documents\\2017-10-20_MEA2_100000rows_10sec";
+        dbHandler.insertCSVMatrixIntoDB();
     }
 
 
--- src/main/java/utilities/DBHandler.java
+++ src/main/java/utilities/DBHandler.java
@@ -1,14 +1,30 @@
 package utilities;
 
+import org.hibernate.validator.cfg.context.ContainerElementConstraintMappingContext;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class DBHandler {
     private final String username = "kimera_k3ah";
     private final String  password = "passord132";
-    private final String url = "mysql.stud.ntnu.no";
-    private final String driver = "com.mysql.jdbc.Driver";
+    //private final String url = "mysql.stud.ntnu.no";
+    private final String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String url = "jdbc:mysql://mysql.stud.ntnu.no:3306/kimera_k3ah";
 
+    public DBHandler(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            System.out.println("Could not find driver for database");
+        }
+    }
 
     public Connection getConnection(){
         Connection conn = null;
@@ -18,26 +34,81 @@ public class DBHandler {
             ex.getErrorCode();
             ex.getSQLState();
         }
-            System.out.println("Connected to database");
+        if(conn == (null)){
+            System.out.println("Could not create connection");
+            return null;
+        }
+        System.out.println("Connected to database");
         return conn;
 
     }
 
+    public boolean insertCSVMatrixIntoDB(){
+        FileDialog dialog = new FileDialog((Frame)null, "Select File to Open");
+        dialog.setMode(FileDialog.LOAD);
+        dialog.setVisible(true);
+        String file = dialog.getFile();
+        String directory = dialog.getDirectory();
+        System.out.println(file + " chosen.");
+        System.out.println(directory + " directory");
+        String query = "INSERT into cellData (cellID, timeStamp, value) values (?, ?, ?)";
+
+        try{
+
+            Connection conn = getConnection();
+            PreparedStatement statement = conn.prepareStatement(query);
+            ResultSet rs = null;
+
+            Scanner scanner = new Scanner(new File(directory+file));
+            String[] cellIDArray = scanner.nextLine().split(",");
+            String timestamp = "";
+            int counter = 0;
+            System.out.println("Starting read of file");
+            System.out.println(cellIDArray[0] + cellIDArray[1]);
+            while(scanner.hasNextLine()){
+                System.out.println("Starting scan");
+                String line = scanner.nextLine();
+                String[] values = line.split(",");
+                timestamp = values[0];
+                for (int x = 1; x < values.length; x++){
+                    System.out.println((x-1) + " values processed");
+                    statement.setString(1,cellIDArray[x]);
+                    statement.setString(2, timestamp);
+                    statement.setString(3, values[x]);
+                    statement.execute();
+                }
+                counter++;
+                System.out.println(counter + " lines processed");
+            }
+            return true;
+
+
+
+        }catch (SQLException ex){
+            System.out.println("SQL error");
+            ex.getErrorCode();
+            ex.getSQLState();
+            return false;
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     public List<String> selectAllFromTable(String table) {
-        Connection conn = getConnection();
+        Connection conn = this.getConnection();
+        System.out.println(conn.toString());
         Statement stmt = null;
         ResultSet rs = null;
 
+        List<String> results = new ArrayList<>();
         try {
             stmt = conn.createStatement();
-            rs = stmt.executeQuery("SELECT foo FROM bar");
-
-            // or alternatively, if you don't know ahead of time that
-            // the query will be a SELECT...
-
-            if (stmt.execute("SELECT foo FROM bar")) {
-                rs = stmt.getResultSet();
+            rs = stmt.executeQuery("SELECT * FROM "+table);
+            while (rs.next()) {
+                System.out.println(rs.getInt(1));
             }
+
             return null;
 
 
